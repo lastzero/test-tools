a96b91d4251bdc4e72cda4eb25cd54ffe23abc65
Michael Mayer <michael@lastzero.net>
2016-09-30 11:09:14 +0200

    Added Swift Mailer test double

---
+++ src/TestTools/Swift/Mailer.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace TestTools\Swift;
+
+use Swift_Mailer;
+use Swift_Mime_Message;
+
+/**
+ * Test double for Swift Mailer
+ *
+ * @author Michael Mayer <michael@lastzero.net>
+ * @license MIT
+ */
+class Mailer extends Swift_Mailer
+{
+    /**
+     * Send the given Message like it would be sent in a mail client.
+     *
+     * All recipients (with the exception of Bcc) will be able to see the other
+     * recipients this message was sent to.
+     *
+     * Recipient/sender data will be retrieved from the Message object.
+     *
+     * The return value is the number of recipients who were accepted for
+     * delivery.
+     *
+     * @param Swift_Mime_Message $message
+     * @param array $failedRecipients An array of failures by-reference
+     *
+     * @return int The number of successful recipients. Can be 0 which indicates failure
+     */
+    public function send(Swift_Mime_Message $message, &$failedRecipients = null)
+    {
+        file_put_contents($this->getTempFilename(), serialize($message));
+
+        $result = parent::send($message, $failedRecipients);
+
+        return $result;
+    }
+
+    /**
+     * Returns file name for storing last message in temp directory
+     *
+     * @return string
+     */
+    protected function getTempFilename()
+    {
+        $result = sys_get_temp_dir() . '/mail-' . md5(__FILE__) . '.ser';
+
+        return $result;
+    }
+
+    /**
+     * Returns the last message sent
+     *
+     * @return Swift_Mime_Message
+     */
+    public function getLastMessage()
+    {
+        $result = unserialize(file_get_contents($this->getTempFilename()));
+
+        return $result;
+    }
+
+    /**
+     * Deletes the last message from temp directory
+     */
+    public function clearLastMessage()
+    {
+        $filename = $this->getTempFilename();
+
+        if(file_exists($filename)) {
+            unlink($filename);
+        }
+    }
+}
---
+++ src/TestTools/Tests/Swift/MailerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace TestTools\Tests\Swift;
+
+use TestTools\TestCase\UnitTestCase;
+use Swift_Message as Message;
+
+/**
+ * @author Michael Mayer <michael@lastzero.net>
+ * @package TestTools
+ * @license MIT
+ */
+class MailerTest extends UnitTestCase
+{
+    /**
+     * @var \TestTools\Swift\Mailer
+     */
+    protected $mailer;
+
+    public function setUp()
+    {
+        $this->mailer = $this->get('swift.mailer');
+    }
+
+    public function testSend()
+    {
+        $message = Message::newInstance();
+
+        $message->setSubject('This is a test mail')
+            ->setFrom('sender@example.com')
+            ->setTo(array('test@example.com', 'foo@bar.com'))
+            ->setBody('Mail Contents');
+
+        $result = $this->mailer->send($message);
+
+        $this->assertEquals(2, $result);
+
+        $lastMessage = $this->mailer->getLastMessage();
+
+        $this->assertEquals('text/plain', $lastMessage->getContentType());
+        $this->assertEquals('Mail Contents', $lastMessage->getBody());
+        $this->assertEquals('This is a test mail', $lastMessage->getSubject());
+        $this->assertEquals(array('sender@example.com' => null), $lastMessage->getFrom());
+
+        $this->mailer->clearLastMessage();
+    }
+}
